9baeb0e5f99eb096e31022cd21b20da70631479c
leannecornish-ft <Leanne.Cornish@ft.com>
2021-01-14 18:41:16 +0000

    fixed spacing

--- requestMultipleUrls.js
+++ requestMultipleUrls.js
@@ -1,6 +1,5 @@
 const fetch = require('node-fetch');
 
-
 //Check returned promise is ok, otherwise throw error 
 function checkStatus(res) {
     if (res.ok) {
@@ -9,7 +8,6 @@ function checkStatus(res) {
         throw new Error(`${res.status} ${res.statusText}`);
     };
 };
-
 
 function requestMultipleUrls(arr) { 
     //accept only arrays
@@ -28,8 +26,5 @@ function requestMultipleUrls(arr) {
     //return fetched content as a promise
     return Promise.all(promises).catch(error => `One or more URLs failed to load with the following ${error}`);
 };
-
-
-  
 
 module.exports = requestMultipleUrls
--- test.js
+++ test.js
@@ -2,8 +2,6 @@ const { expect } = require('chai');
 const chai = require('chai');
 const { assert } = require('chai')
 const chaiAsPromised = require('chai-as-promised');
-
-
 const requestMultipleUrls = require('./requestMultipleUrls');
 
 chai.use(chaiAsPromised)
@@ -20,22 +18,16 @@ describe('Global', () => {
             return assert.isFulfilled(Promise.resolve(requestMultipleUrls(urls)))
         })
 
-
         it('Returns an array of contents', function() {    
             let result = requestMultipleUrls(urls)
-            return expect(result).to.eventually.be.a('array')
-      
+            return expect(result).to.eventually.be.a('array')   
         });
     })
 
     describe('Function given bad data', function() {
-
         it('Returns a meaningful error if a URL does not load', function() {
             result = requestMultipleUrls([1,2,3])
             return expect(result).to.eventually.contain('One or more URLs failed to load with the following')
         });
-
     });
-
 });
-
